72dfe56a6958fca12a01688bdfbd4a4cd3dab536
Adrian Lang <mail@adrianlang.de>
2013-02-06 13:14:52 +0100

    Update package.json, use mocha for tests

--- test/tests.js
+++ test/tests.js
@@ -1,8 +1,10 @@
 "use strict";
 
-var lib = require("lib");
+var assert = require('assert');
+var lib = require("../lib");
 
-exports.firstRes = function (beforeExit, assert) {
+describe('firstRes', function () {
+  it('returns the expected results', function () {
     assert.strictEqual(lib.firstRes(['focus', 'tab'], function (type) {
         return (type === 'focus') && type;
     }), 'focus');
@@ -16,10 +18,9 @@ exports.firstRes = function (beforeExit, assert) {
     assert.strictEqual(lib.firstRes([0, 1, 2], function (type) {
         return type + 5;
     }), 5);
-};
+  });
 
-// Make sure that firstRes passes fail types from the inner function
-exports.firstResFailTypes = function (beforeExit, assert) {
+  it('passes fail types from the inner function', function () {
     assert.strictEqual(lib.firstRes(['focus', 'tab'], function (type) {
         return (type === '__none__') && type;
     }), false);
@@ -31,4 +32,5 @@ exports.firstResFailTypes = function (beforeExit, assert) {
     assert.strictEqual(lib.firstRes([{}, {}], function (v) {
         return v.x;
     }), undefined);
-};
+  });
+});
