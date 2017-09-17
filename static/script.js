990f447ba51fde28791324dfe78d95ffc9f09d82
Adrian Heine <mail@adrianheine.de>
2017-09-17 17:37:20 +0200

    Remove ender

--- static/script.js
+++ static/script.js
@@ -16,42 +16,42 @@
         mime = 'application/prs.de.adrianlang.hire.tab';
       }
 
-      $.ajax({
-        url: url,
-        // I want to get the response object, so I need to pass a
-        // non-existing type so that reqwest doesn't do anything clever
-        type: '__non_existing__',
-        headers: { 'Accept': mime }
-      }).then(function (resp) {
+      var oReq = new XMLHttpRequest();
+      oReq.addEventListener("load", () => {
         // Make sure that we got the right thing
-        var received_mime = resp.getResponseHeader('Content-Type');
+        var received_mime = oReq.getResponseHeader('Content-Type');
         if (received_mime.substr(0, mime.length + 1) !== mime + ';') {
           // If not, fall back to plain page loading
           document.location = url;
           return;
         }
 
-        target.innerHTML = resp.responseText;
+        target.innerHTML = oReq.responseText;
 
         if (title) {
           document.title = title;
         }
 
-        var $nav = $('nav');
-        $nav.find('a.active').removeClass('active');
-        $nav.find('a[href="/' + (url_parsed[1] || 'intro') + '"]').addClass('active');
+        let active_tab = document.querySelector('nav a.active');
+        if (active_tab) active_tab.classList.remove('active');
+        if (url_parsed[1])
+          document.querySelector('nav a[href="/' + url_parsed[1] + '"]').classList.add('active');
 
         last_loaded = url_parsed[1];
-      }, function () {
+      });
+      oReq.addEventListener('error', function () {
         document.location = url;
       });
+      oReq.open('GET', url);
+      oReq.setRequestHeader('Accept', mime);
+      oReq.send();
 
       return true;
     };
   }());
 
   // From https://github.com/defunkt/jquery-pjax/blob/master/jquery.pjax.js
-  $(window).on('popstate', function () {
+  window.addEventListener('popstate', function () {
     var popped = typeof history.state !== 'undefined',
       initialURL = location.href;
     return function (e) {
@@ -70,24 +70,26 @@
     };
   }());
 
-  $.domReady(function () {
-    $(document.body).on('click', 'a[href^=/]', function (e) {
-      var url = this.getAttribute('href'),
-        title = this.title;
-      if (e.ctrlKey || e.shiftKey || e.altKey) {
-        return;
-      }
+  document.body.addEventListener('click', function (e) {
+    var a = e.target;
+    while (a && a.tagName !== 'A') a = a.parentNode;
+    if (!a) return;
+    var url = a.getAttribute('href');
+    if (!url.match(/^\//)) return;
+    if (e.ctrlKey || e.shiftKey || e.altKey) {
+      return;
+    }
 
-      if (!load(url, title)) {
-        return;
-      }
-      history.pushState({url: url, title: title}, title, url);
-      e.preventDefault();
-    });
-
-    history.replaceState({
-      url: String(document.location.pathname),
-      title: String(document.title)
-    }, String(document.title));
+    var title = a.title
+    if (!load(url, title)) {
+      return;
+    }
+    history.pushState({url: url, title: title}, title, url);
+    e.preventDefault();
   });
+
+  history.replaceState({
+    url: String(document.location.pathname),
+    title: String(document.title)
+  }, String(document.title));
 }());
